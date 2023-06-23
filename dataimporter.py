2df1f61aae09dd841055170dea730d3022594760
Henrique <henrique.ramalho2002@gmail.com>
2023-06-22 22:44:30 -0300

    Fixing csv path

--- dataimporter.py
+++ dataimporter.py
@@ -1,10 +1,12 @@
 import MySQLdb
 import csv
+import os
 
 class DataImporter:
 
     def __init__(self, database):
         self.db = MySQLdb.connect("localhost", "root", "1234", database)
+        self.databaseName = database
 
     def load(self):
         cursor = self.db.cursor()
@@ -16,14 +18,19 @@ class DataImporter:
             results = cursor.fetchall()
 
             column_names = [description[0] for description in cursor.description]
+            path = "csv/" + self.databaseName
 
-            csv_filename = f"{table_name}.csv"
+            if not os.path.exists(path) :
+                os.makedirs(path)
+
+            filename = f"{table_name}.csv"
+            csv_filename = path + "/" + filename
+
             with open(csv_filename, 'w', newline='') as csvfile:
-                writer = csv.writer(csvfile)
+                writer = csv.writer(csvfile, delimiter=';')
                 
                 # Escrever os nomes das colunas
                 writer.writerow(column_names)
                 
                 # Escrever os dados
                 writer.writerows(results)
-    
