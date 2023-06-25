02fbb336faf29cfe7c123499cbda5015c3ce8b13
Henrique <henrique.ramalho2002@gmail.com>
2023-06-25 00:05:11 -0300

    Fixing exit command

--- querymenu.py
+++ querymenu.py
@@ -6,20 +6,22 @@ class QueryMenu(State):
     def __init__(self, app, database):
         super().__init__(app)
         self.database = database
+        self.quit = False
 
     def processInput(self):
 
         self.clearTerminal()
 
-        while 1:
+        while not self.quit:
             print()
             print("Digite a query:")
             query = input()
             if query == "exit":
-                from loadeddatabasesmenu import LoadedDatabasesMenu
-                self.app.changeState(LoadedDatabasesMenu(self.app))
+               self.quit = True 
             else:
                 print()
                 processor = QueryProcessor(self.database)
                 processor.processQuery(self.database, query)
         
+        from loadeddatabasesmenu import LoadedDatabasesMenu
+        self.app.changeState(LoadedDatabasesMenu(self.app))
